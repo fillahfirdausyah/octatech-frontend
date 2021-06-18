b6d930e38497357c90437058e16145da6bf8947a
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-06-18 19:55:51 +0700

    fix bug on Gallery Component

--- src/Component/Gallery/index.js
+++ src/Component/Gallery/index.js
@@ -5,15 +5,15 @@ import "./style.css";
 
 function Gallery() {
   return (
-    <div className="gallery">
-      <h1>Our Gallery</h1>
+    <div className="gallery text-center"> 
+      <h1>GALLERY KAMI</h1>
       <div class="row">
         <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
           <img
             data-aos="fade-up"
             data-aos-duration="900"
             src="https://1.bp.blogspot.com/-Q7TtokntPlU/XygeuiB-XfI/AAAAAAAAHHA/jZIUkMo-ItwClmoYhf8x0Qf6SYrEicpxgCLcBGAsYHQ/s1600/1.gif"
-            class="w-100 shadow-1-strong rounded mb-4"
+            class="w-100 shadow-1-strong mb-4"
             alt=""
           />
 
@@ -21,7 +21,7 @@ function Gallery() {
             data-aos="fade-up"
             data-aos-duration="900"
             src="https://i.pinimg.com/originals/63/18/d4/6318d42c351ddffaff82568d9093f33d.jpg"
-            class="w-100 shadow-1-strong rounded mb-4"
+            class="w-100 shadow-1-strong mb-4"
             alt=""
           />
         </div>
@@ -31,7 +31,7 @@ function Gallery() {
             data-aos="fade-up"
             data-aos-duration="900"
             src="https://64.media.tumblr.com/86ebb426f1127c1210e7b2d5c909e7a3/009bb117a480df96-bb/s640x960/7d244e3089e26fd76dbfea7dea37eec30b805ead.png"
-            class="w-100 shadow-1-strong rounded mb-4"
+            class="w-100 shadow-1-strong mb-4"
             alt=""
           />
 
@@ -39,7 +39,7 @@ function Gallery() {
             data-aos="fade-up"
             data-aos-duration="900"
             src="https://i.pinimg.com/474x/88/3f/51/883f5115db5e39bd9cc9474207fce230.jpg"
-            class="w-100 shadow-1-strong rounded mb-4"
+            class="w-100 shadow-1-strong mb-4"
             alt=""
           />
         </div>
@@ -49,14 +49,14 @@ function Gallery() {
             data-aos="fade-up"
             data-aos-duration="900"
             src="https://www.lovinkproject.com/wp-content/uploads/2021/03/d84f710f52006da3baac6b6727e8b8a0.jpg"
-            class="w-100 shadow-1-strong rounded mb-4"
+            class="w-100 shadow-1-strong mb-4"
             alt=""
           />
           <img
             data-aos="fade-up"
             data-aos-duration="900"
             src="https://dbkpop.com/wp-content/uploads/cache/2021/03/Weeekly_After_School_Jihan_3/3944390263.jpg"
-            class="w-100 shadow-1-strong rounded mb-4"
+            class="w-100 shadow-1-strong mb-4"
             alt=""
           />
         </div>
--- src/Page/GalleryPage/index.js
+++ src/Page/GalleryPage/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
+// Asset
+import './style.css'
+
 // Component
 import Navbar from "../../Component/Navbar";
 import Gallery from "../../Component/Gallery";
@@ -9,6 +12,7 @@ import Footer from "../../Component/Footer";
 function GalleryPage() {
   return (
     <>
+      <div className="dotted d-o galeri"></div>
       <Container>
         <Navbar />
         <Gallery />
