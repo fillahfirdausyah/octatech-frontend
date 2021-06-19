99619cabc37fccba04d4b12fe573cca0eb21b592
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-06-19 18:55:24 +0700

    user can send message

--- src/Component/ContactUs/index.js
+++ src/Component/ContactUs/index.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Toast } from "react-bootstrap";
+import axios from "axios";
 
 // Asset
 import "./style.css";
@@ -7,6 +9,38 @@ import EmailIcon from "@material-ui/icons/Email";
 import PhoneIcon from "@material-ui/icons/Phone";
 
 function ContactUs() {
+  const [showToast, setShowToast] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const emailRef = useRef();
+  const pesanRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const firstName = firstNameRef.current.value;
+    const lastName = lastNameRef.current.value;
+    const email = emailRef.current.value;
+    const pesan = pesanRef.current.value;
+
+    const name = `${firstName} ${lastName}`;
+
+    const data = {
+      name,
+      email,
+      pesan,
+    };
+
+    axios.post("http://localhost:8000/pesan/add", data).then((res) => {
+      setMessage(res.data.message);
+      setShowToast(true);
+    });
+
+    e.target.reset();
+  };
+
   return (
     <div className="contact-us text-center">
       <h1>KONTAK KAMI</h1>
@@ -55,36 +89,58 @@ function ContactUs() {
       </div>
 
       <div className="contact-form">
+        <Toast
+          show={showToast}
+          onClose={() => setShowToast(false)}
+          delay={2000}
+          autohide
+        >
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded me-2"
+              alt=""
+            />
+            <strong className="me-auto">Message</strong>
+          </Toast.Header>
+          <Toast.Body>{message}</Toast.Body>
+        </Toast>
         <h1>Kirim Pesan</h1>
         <div className="row" data-aos="fade-up" data-aos-duration="900">
           <div className="col">
-            <form>
+            <form onSubmit={submitHandler}>
               <div className="mb-3">
                 <div className="row">
                   <div className="col">
                     <input
+                      required
                       type="text"
                       class="form-control"
                       id="fist-name"
                       placeholder="Nama Depan"
+                      ref={firstNameRef}
                     />
                   </div>
                   <div className="col">
                     <input
-                      type="email"
+                      required
+                      type="text"
                       class="form-control"
                       id="last-name"
                       placeholder="Nama Belakang"
+                      ref={lastNameRef}
                     />
                   </div>
                 </div>
               </div>
               <div class="mb-3">
                 <input
+                  required
                   type="email"
                   class="form-control"
                   id="email"
                   placeholder="name@example.com"
+                  ref={emailRef}
                 />
               </div>
               <div class="mb-3">
@@ -93,6 +149,7 @@ function ContactUs() {
                   id="exampleFormControlTextarea1"
                   placeholder="Pesan Kamu"
                   rows="3"
+                  ref={pesanRef}
                 ></textarea>
               </div>
               <button className="btn btn-primary w-100">Kirim Pesan</button>
